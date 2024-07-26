bb8b4c283f5b304d2d7f92711534f92c8a25d9a8
levi <425714149@qq.com>
2024-07-26 15:29:59 +0800

    add include, src;add 动态库

--- planning_main.cpp
+++ planning_main.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-    std::cout<<"planning start"<<std::endl;
+    std::cout<<"planning start!"<<std::endl;
     Process pro;
     pro.planProcess();
-    std::cout<<"planning end"<<std::endl;   
+    std::cout<<"planning end!2142142142121421"<<std::endl;   
 
     return 0;
 }
